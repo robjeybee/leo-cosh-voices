fe830d7f927cdf4168d98c35a9a8d03afa40136c
Rob B <rob@rjrobbie.dev>
2025-10-19 11:23:25 +0100

    added demo reel section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,29 +3,28 @@ import "../styles/hero.css";
 
 function Hero() {
   return (
-    <>
-      <section className="hero">
-        <div className="hero-container">
-          <h1 className="hero-title">
-            I'm <span className="hero-name">Leo, </span>
-            <img
-              src="/Assets/Leo Cosh Hero Image.png"
-              alt="Leo portrait"
-              className="hero-inline-image"
-            />
-            <br />a <span>Voice Actor</span>
-            <br />
-            based in <span>London</span>
-          </h1>
-          <p className="hero-subtitle">
-            Your story deserves to be heard,
-            <br />
-            with a voice that keeps people listening.
-          </p>
-          <Button />
-        </div>
-      </section>
-    </>
+    <section className="hero">
+      <div className="hero-container">
+        <h1 className="hero-title">
+          I'm <span className="hero-name">Leo, </span>
+          <img
+            src="/Assets/Leo Cosh Hero Image.png"
+            alt="Portrait of Leo Cosh"
+            className="hero-inline-image"
+            loading="lazy"
+          />
+          <br />a <span>Voice Actor</span>
+          <br />
+          based in <span>London</span>
+        </h1>
+        <p className="hero-subtitle">
+          Your story deserves to be heard,
+          <br />
+          with a voice that keeps people listening.
+        </p>
+        <Button />
+      </div>
+    </section>
   );
 }
 
